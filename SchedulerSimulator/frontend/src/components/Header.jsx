9d9ccac2e4d0e38daeb4d1cce5932dde025cf26f
luz <luz@gmail.com>
2025-09-25 01:10:08 -0300

    🌱 Termino frontend 🌱

---
+++ SchedulerSimulator/frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div className="text-center p-3">
+            <h1 className="text-4xl">Planificación de Procesos</h1>
+        </div>
+        
+    )
+}
+export default Header
